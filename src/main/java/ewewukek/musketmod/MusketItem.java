93ddb1b8283db139112cef910318b870ef7d67fd
ewewukek <ewewukek@gmail.com>
2021-03-30 01:40:37 +0300

    musket item stub with sounds

---
+++ src/main/java/ewewukek/musketmod/MusketItem.java
@@ -0,0 +1,213 @@
+package ewewukek.musketmod;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class MusketItem extends Item {
+    public static final int DURABILITY = 250;
+    public static final int LOADING_STAGE_1 = 5;
+    public static final int LOADING_STAGE_2 = 10;
+    public static final int LOADING_STAGE_3 = 20;
+    public static final int RELOAD_DURATION = 30;
+
+    public static float bulletStdDev;
+    public static double bulletSpeed;
+
+    public MusketItem(Settings settings) {
+        super(settings.maxDamage(DURABILITY));
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World worldIn, PlayerEntity player, Hand hand) {
+        if (hand != Hand.MAIN_HAND) return super.use(worldIn, player, hand);
+
+        ItemStack stack = player.getStackInHand(hand);
+        boolean creative = player.abilities.creativeMode;
+
+        if (player.isSubmergedIn(FluidTags.WATER) && !creative) {
+            return TypedActionResult.fail(stack);
+        }
+
+        boolean haveAmmo = !findAmmo(player).isEmpty() || creative;
+        boolean loaded = isLoaded(stack);
+
+        if (loaded && isReady(stack)) {
+            if (!worldIn.isClient) {
+                fireBullet(worldIn, player);
+            }
+            player.playSound(MusketMod.SOUND_MUSKET_FIRE, 1.5f, 1);
+
+            damageItem(stack, player);
+            setReady(stack, false);
+            setLoaded(stack, false);
+
+            return TypedActionResult.consume(stack);
+
+        } else if (loaded || haveAmmo) {
+            if (!loaded) {
+                setLoadingStage(stack, 0);
+            }
+            player.setCurrentHand(hand);
+            return TypedActionResult.consume(stack);
+
+        } else {
+            return TypedActionResult.fail(stack);
+        }
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
+        if (isLoaded(stack)) setReady(stack, true);
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity entity, ItemStack stack, int timeLeft) {
+        if (world.isClient || !(entity instanceof PlayerEntity)) return;
+
+        PlayerEntity player = (PlayerEntity) entity;
+        int usingDuration = getMaxUseTime(stack) - timeLeft;
+        int loadingStage = getLoadingStage(stack);
+
+        double posX = player.getX();
+        double posY = player.getY();
+        double posZ = player.getZ();
+
+        if (loadingStage == 0 && usingDuration >= LOADING_STAGE_1) {
+            world.playSound(null, posX, posY, posZ, MusketMod.SOUND_MUSKET_LOAD_0, SoundCategory.PLAYERS, 0.5f, 1);
+            setLoadingStage(stack, 1);
+
+        } else if (loadingStage == 1 && usingDuration >= LOADING_STAGE_2) {
+            world.playSound(null, posX, posY, posZ, MusketMod.SOUND_MUSKET_LOAD_1, SoundCategory.PLAYERS, 0.5f, 1);
+            setLoadingStage(stack, 2);
+
+        } else if (loadingStage == 2 && usingDuration >= LOADING_STAGE_3) {
+            world.playSound(null, posX, posY, posZ, MusketMod.SOUND_MUSKET_LOAD_2, SoundCategory.PLAYERS, 0.5f, 1);
+            setLoadingStage(stack, 3);
+        }
+
+        if (usingDuration >= RELOAD_DURATION && !isLoaded(stack)) {
+            if (!player.abilities.creativeMode) {
+                ItemStack ammoStack = findAmmo(player);
+                if (ammoStack.isEmpty()) return;
+
+                ammoStack.decrement(1);
+                if (ammoStack.isEmpty()) player.inventory.removeOne(ammoStack);
+            }
+
+            world.playSound(null, posX, posY, posZ, MusketMod.SOUND_MUSKET_READY, SoundCategory.PLAYERS, 0.5f, 1);
+            setLoaded(stack, true);
+        }
+   }
+
+    @Override
+    public boolean postMine(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        if (!worldIn.isClient && entityLiving instanceof PlayerEntity && state.getHardness(worldIn, pos) != 0.0f) {
+            damageItem(stack, (PlayerEntity) entityLiving);
+        }
+        return false;
+    }
+
+    public static void damageItem(ItemStack stack, PlayerEntity player) {
+        stack.damage(1, player, (entity) -> {
+            entity.sendToolBreakStatus(player.getActiveHand());
+        });
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 72000;
+    }
+
+    public static boolean isLoaded(ItemStack stack) {
+        CompoundTag tag = stack.getTag();
+        return tag != null && tag.getByte("loaded") == 1;
+    }
+
+    public static boolean isReady(ItemStack stack) {
+        CompoundTag tag = stack.getTag();
+        return tag != null && tag.getByte("ready") == 1;
+    }
+
+    private boolean isAmmo(ItemStack stack) {
+        return stack.getItem() == MusketMod.CARTRIDGE;
+    }
+
+    private ItemStack findAmmo(PlayerEntity player) {
+        if (isAmmo(player.getStackInHand(Hand.OFF_HAND))) {
+            return player.getStackInHand(Hand.OFF_HAND);
+
+        } else if (isAmmo(player.getStackInHand(Hand.MAIN_HAND))) {
+            return player.getStackInHand(Hand.MAIN_HAND);
+
+        } else {
+            for (int i = 0; i != player.inventory.main.size(); ++i) {
+                ItemStack itemstack = player.inventory.main.get(i);
+                if (isAmmo(itemstack)) return itemstack;
+            }
+
+            return ItemStack.EMPTY;
+        }
+    }
+
+    private Vec3d getPlayerFiringPoint(PlayerEntity player) {
+        Vec3d side = Vec3d.fromPolar(0, player.yaw + 90);
+        if (player.getActiveHand() == Hand.OFF_HAND) side = side.multiply(-1);
+        Vec3d down = Vec3d.fromPolar(player.pitch + 90, player.yaw);
+
+        return new Vec3d(player.getX(), player.getEyeY(), player.getZ())
+                .add(side.add(down).multiply(0.1));
+    }
+
+    private void fireBullet(World worldIn, PlayerEntity player) {
+        Vec3d pos = getPlayerFiringPoint(player);
+        Vec3d front = Vec3d.fromPolar(player.pitch, player.yaw);
+
+        float angle = (float) Math.PI * 2 * RANDOM.nextFloat();
+        float gaussian = Math.abs((float) RANDOM.nextGaussian());
+        if (gaussian > 4) gaussian = 4;
+
+        front = front.rotateX(bulletStdDev * gaussian * MathHelper.sin(angle))
+                .rotateY(bulletStdDev * gaussian * MathHelper.cos(angle));
+
+        Vec3d motion = front.multiply(bulletSpeed);
+
+        Vec3d playerMotion = player.getVelocity();
+        motion.add(playerMotion.x, player.isOnGround() ? 0 : playerMotion.y, playerMotion.z);
+
+//        BulletEntity bullet = new BulletEntity(worldIn);
+//        bullet.setShooter(player);
+//        bullet.setPosition(pos.x, pos.y, pos.z);
+//        bullet.setMotion(motion);
+//        bullet.doFireParticles = true;
+//
+//        worldIn.addEntity(bullet);
+    }
+
+    private void setLoaded(ItemStack stack, boolean loaded) {
+        stack.getOrCreateTag().putByte("loaded", (byte) (loaded ? 1 : 0));
+    }
+
+    private void setReady(ItemStack stack, boolean ready) {
+        stack.getOrCreateTag().putByte("ready", (byte) (ready ? 1 : 0));
+    }
+
+    private void setLoadingStage(ItemStack stack, int loadingStage) {
+        stack.getOrCreateTag().putInt("loadingStage", loadingStage);
+    }
+
+    private int getLoadingStage(ItemStack stack) {
+        return stack.getOrCreateTag().getInt("loadingStage");
+    }
+}
--- src/main/java/ewewukek/musketmod/MusketMod.java
+++ src/main/java/ewewukek/musketmod/MusketMod.java
@@ -3,6 +3,7 @@ package ewewukek.musketmod;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,7 +13,13 @@ public class MusketMod implements ModInitializer {
     public static final Item BARREL = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item STOCK = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item CARTRIDGE = new Item(new Item.Settings().group(ItemGroup.COMBAT));
-    public static final Item MUSKET = new Item(new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item MUSKET = new MusketItem(new Item.Settings().group(ItemGroup.COMBAT));
+
+    public static final SoundEvent SOUND_MUSKET_LOAD_0 = new SoundEvent(new Identifier(MODID, "musket_load0"));
+    public static final SoundEvent SOUND_MUSKET_LOAD_1 = new SoundEvent(new Identifier(MODID, "musket_load1"));
+    public static final SoundEvent SOUND_MUSKET_LOAD_2 = new SoundEvent(new Identifier(MODID, "musket_load2"));
+    public static final SoundEvent SOUND_MUSKET_READY = new SoundEvent(new Identifier(MODID, "musket_ready"));
+    public static final SoundEvent SOUND_MUSKET_FIRE = new SoundEvent(new Identifier(MODID, "musket_fire"));
 
     @Override
     public void onInitialize() {
@@ -20,5 +27,11 @@ public class MusketMod implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MODID, "stock"), STOCK);
         Registry.register(Registry.ITEM, new Identifier(MODID, "cartridge"), CARTRIDGE);
         Registry.register(Registry.ITEM, new Identifier(MODID, "musket"), MUSKET);
+
+        Registry.register(Registry.SOUND_EVENT, new Identifier(MODID, "musket_load0"), SOUND_MUSKET_LOAD_0);
+        Registry.register(Registry.SOUND_EVENT, new Identifier(MODID, "musket_load1"), SOUND_MUSKET_LOAD_1);
+        Registry.register(Registry.SOUND_EVENT, new Identifier(MODID, "musket_load2"), SOUND_MUSKET_LOAD_2);
+        Registry.register(Registry.SOUND_EVENT, new Identifier(MODID, "musket_ready"), SOUND_MUSKET_READY);
+        Registry.register(Registry.SOUND_EVENT, new Identifier(MODID, "musket_fire"), SOUND_MUSKET_FIRE);
     }
 }
